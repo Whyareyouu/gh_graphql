f81dcedd902a8c9de480b6c25450e76fac3152fa
whyareyou <bionixxxd5@gmail.com>
2024-11-25 17:20:53 +0000

    style: change layout width

--- src/components/ui/layout.tsx
+++ src/components/ui/layout.tsx
@@ -4,7 +4,7 @@ import { Header } from "./header";
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col w-[1320px]">
+      <div className="flex flex-col w-[1600px]">
         <Header />
         <main>{children}</main>
         <Footer />
